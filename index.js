71ae89491d821d34d97ca69c37e46ff7f13b8598
Adam Cox <adamcox.dev@gmail.com>
2022-09-21 15:30:53 -0600

    Trying css again

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'))
+    res.sendFile(path.join(__dirname, './index.html/styles.css'))
 })
 
 app.listen(port, () => {
