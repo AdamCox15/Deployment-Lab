9616688ce129555446ea11d2a01a5d3d7d1e687b
Adam Cox <adamcox.dev@gmail.com>
2022-09-21 15:34:20 -0600

    Trying something to link css

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './styles.css'))
+})
+
 app.listen(port, () => {
     console.log('Running on ' + port)
 })
