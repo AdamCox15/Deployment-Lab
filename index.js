8fff23d31d07d53a0fa097978d71b8cde28a43d4
Adam Cox <adamcox.dev@gmail.com>
2022-09-21 15:08:32 -0600

    First commit on lab

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const path = require('path');
+
+require('dotenv').config()
+
+const port = process.env.PORT || 6000
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(port, () => {
+    console.log('Running on ' + port)
+})
